1a9935c96ae797bc73eff4827563a7e694043dbc
vinhpha111 <vinhpha111@gmail.com>
2023-04-27 14:45:43 +0700

    update

---
+++ Object Repository/Page_Bo Tui Tr - Tin tc mi nht, tin nhanh, _3fc11d/a_Php lut.rs
@@ -0,0 +1,150 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Php lut</name>
+   <tag></tag>
+   <elementGuidId>31a171ff-4ebd-434f-a075-d7850d1e0719</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='admWrapsite']/div/div[2]/div/div[2]/div/div/div/div/ul/li[5]/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>6f9d8a50-2c10-4329-aa86-b46cef96317c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>title</name>
+      <type>Main</type>
+      <value>Pháp luật</value>
+      <webElementGuid>9dc4afc7-6da3-43e0-ab79-ee66609b3fe2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/phap-luat.htm</value>
+      <webElementGuid>795be7a1-029c-420c-9fa2-fc6c3eff0734</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>nav-link </value>
+      <webElementGuid>2015481f-f5f2-4d4b-ae5c-f382347a65ed</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Pháp luật
+                                    </value>
+      <webElementGuid>c02124ea-4dcb-4798-9bee-b72cac7b2296</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;admWrapsite&quot;)/div[@class=&quot;wapper&quot;]/div[@class=&quot;header&quot;]/div[@class=&quot;header__sc&quot;]/div[@class=&quot;header__middle&quot;]/div[@class=&quot;header__postion&quot;]/div[@class=&quot;header__nav&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;header__nav-flex&quot;]/ul[@class=&quot;menu-nav&quot;]/li[5]/a[@class=&quot;nav-link&quot;]</value>
+      <webElementGuid>fa4136f7-3c07-4600-a185-e879f366471b</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='admWrapsite']/div/div[2]/div/div[2]/div/div/div/div/ul/li[5]/a</value>
+      <webElementGuid>509b6679-bdc4-4cfb-b5a2-68579cbd7d2a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Pháp luật')]</value>
+      <webElementGuid>ae2fb2f1-47b6-4fee-8dde-fa4b669c8aa4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Thế giới'])[1]/following::a[1]</value>
+      <webElementGuid>9d2ec353-1b95-4839-9457-b30d2f1b1c21</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Thời sự'])[1]/following::a[2]</value>
+      <webElementGuid>e4d23a71-d930-4ac6-9391-1ae8a99e9700</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Kinh doanh'])[1]/preceding::a[1]</value>
+      <webElementGuid>2c51da99-af4c-4c51-9c00-d828e8b81b5f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Công nghệ'])[1]/preceding::a[2]</value>
+      <webElementGuid>139467a0-f3c7-45cf-99c5-d40998ea242c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Pháp luật']/parent::*</value>
+      <webElementGuid>274e25ad-9844-494d-8945-831a8d5d0361</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/phap-luat.htm')]</value>
+      <webElementGuid>ffc3247d-7b91-443e-8feb-51540cb1bfb7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div/ul/li[5]/a</value>
+      <webElementGuid>ac83c765-edb2-470d-b03a-6d0da6d43746</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@title = 'Pháp luật' and @href = '/phap-luat.htm' and (text() = 'Pháp luật
+                                    ' or . = 'Pháp luật
+                                    ')]</value>
+      <webElementGuid>d9ba92f6-937a-4eee-95f4-eb36858dec0c</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d0857898-689c-4133-bb0c-1ff89624b27c</testSuiteGuid>
+</TestSuiteEntity>
